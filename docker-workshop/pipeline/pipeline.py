565318489531d2f4d072f3638f8db8b3b26a27cf
Danson <dansonkalaghe@gmail.com>
2026-01-18 18:54:31 +0300

    refactor pipeline

--- docker-workshop/pipeline/pipeline.py
+++ docker-workshop/pipeline/pipeline.py
@@ -1,10 +1,5 @@
-import sys
 import pandas as pd
-
-print("arguments", sys.argv)
-
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
+import sys
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
