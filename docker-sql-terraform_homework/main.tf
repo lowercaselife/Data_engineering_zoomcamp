221d788906935c0463cbdd0c0668bed778ea42a2
Danson <dansonkalaghe@gmail.com>
2026-01-27 20:51:35 +0300

    terraform setup

---
+++ docker-sql-terraform_homework/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "lively-antonym-483607-j3"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "lively-antonym-483607-j3-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
